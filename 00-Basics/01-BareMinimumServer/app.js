8bd6191d528aed7dd1f3bf4d469842785d2efd66
Jeff Pottruff <jeffpottruff@gmail.com>
2024-04-05 17:13:14 -0400

    add basic html resposne

--- 00-Basics/01-BareMinimumServer/app.js
+++ 00-Basics/01-BareMinimumServer/app.js
@@ -24,6 +24,14 @@ function killServer() {
 const server = http.createServer((req, res) => {
   // console.log("hello arrow function", req);
   console.log("hello arrow function", req.url, req.method, req.headers);
+
+  // HTML Response
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>My Simple Page</title></head>");
+  res.write("<body><h1>My Page</h1></body>");
+  res.write("</html>");
+  res.end();
 });
 
 server.listen(3000);
