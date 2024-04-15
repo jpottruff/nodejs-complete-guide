73929ac0a42fa0e4f054be952014de0b6e3ae871
Jeff Pottruff <jeffpottruff@gmail.com>
2024-04-15 13:05:55 -0400

    basic middleware example

--- 03-ExpressJS/01-BasicExpressApp/app.js
+++ 03-ExpressJS/01-BasicExpressApp/app.js
@@ -4,5 +4,16 @@ const express = require("express");
 
 const app = express();
 
+// Middleware 1
+app.use((req, res, next) => {
+  console.log("In the middleware");
+  next(); // allows the app to travel on to the next middleware
+});
+
+// Middleware 2
+app.use((req, res, next) => {
+  console.log("In the next middleware");
+});
+
 const server = http.createServer(app);
 server.listen(3000);
