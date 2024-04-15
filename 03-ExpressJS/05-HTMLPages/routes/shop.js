ed1a46ea1c35df2e2b662cdfe7055ac3095ece7d
Jeff Pottruff <jeffpottruff@gmail.com>
2024-04-15 16:33:42 -0400

    remove unncessary slash

--- 03-ExpressJS/05-HTMLPages/routes/shop.js
+++ 03-ExpressJS/05-HTMLPages/routes/shop.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 router.get("/", (req, res, next) => {
   // *NOTE: __dirname is a variable made available by node that exposes the absolute path to the current file
-  res.sendFile(path.join(__dirname, "../", "views", "shop.html"));
+  res.sendFile(path.join(__dirname, "..", "views", "shop.html"));
 });
 
 module.exports = router;
