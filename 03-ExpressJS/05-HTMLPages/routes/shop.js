8f46e7213f156316ee26cda4bb6de7bcff706a59
Jeff Pottruff <jeffpottruff@gmail.com>
2024-04-15 16:15:27 -0400

    add shop.html and serve page

--- 03-ExpressJS/05-HTMLPages/routes/shop.js
+++ 03-ExpressJS/05-HTMLPages/routes/shop.js
@@ -1,8 +1,10 @@
+const path = require("path");
 const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
-  res.send("<h1>Hello from express</h1>");
+  // *NOTE: __dirname is a variable made available by node that exposes the absolute path to the current file
+  res.sendFile(path.join(__dirname, "../", "views", "shop.html"));
 });
 
 module.exports = router;
