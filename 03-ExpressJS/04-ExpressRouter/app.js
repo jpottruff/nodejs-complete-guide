7b7b13e1d1e53879371ab33ca3af0e6ddebde6e4
Jeff Pottruff <jeffpottruff@gmail.com>
2024-04-15 15:27:45 -0400

    add 404 page

--- 03-ExpressJS/04-ExpressRouter/app.js
+++ 03-ExpressJS/04-ExpressRouter/app.js
@@ -11,4 +11,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(adminRoutes);
 app.use(shopRoutes);
 
+app.use((req, res, next) => {
+  res.status(404).send("<h1>PAGE NOT FOUND</h1>");
+});
+
 app.listen(3000);
