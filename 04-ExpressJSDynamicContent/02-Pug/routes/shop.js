6023e42030d14256c9a67b9904733de0d1420846
Jeff Pottruff <jeffpottruff@gmail.com>
2024-04-17 06:48:26 -0400

    add dynamic active class

--- 04-ExpressJSDynamicContent/02-Pug/routes/shop.js
+++ 04-ExpressJSDynamicContent/02-Pug/routes/shop.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 router.get("/", (req, res, next) => {
   const products = adminData.products;
-  res.render("shop", { docTitle: "Shop", prods: products });
+  res.render("shop", { docTitle: "Shop", path: "/", prods: products });
 });
 
 module.exports = router;
