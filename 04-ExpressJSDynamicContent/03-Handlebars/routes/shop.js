f054fb12410079a8e1fe4f0eb2f695eccb7a7195
Jeff Pottruff <jeffpottruff@gmail.com>
2024-04-19 07:56:08 -0400

    add loop through products logic

--- 04-ExpressJSDynamicContent/03-Handlebars/routes/shop.js
+++ 04-ExpressJSDynamicContent/03-Handlebars/routes/shop.js
@@ -5,7 +5,12 @@ const router = express.Router();
 
 router.get("/", (req, res, next) => {
   const products = adminData.products;
-  res.render("shop", { docTitle: "Shop", path: "/", prods: products });
+  res.render("shop", {
+    docTitle: "Shop",
+    path: "/",
+    prods: products,
+    hasProducts: products.length > 0,
+  });
 });
 
 module.exports = router;
