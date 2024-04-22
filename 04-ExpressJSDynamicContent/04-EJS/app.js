acb12d8f6597edf6bf70bdfe17eac0d28aff640d
Jeff Pottruff <jeffpottruff@gmail.com>
2024-04-22 16:00:56 -0400

    render 404 view

--- 04-ExpressJSDynamicContent/04-EJS/app.js
+++ 04-ExpressJSDynamicContent/04-EJS/app.js
@@ -19,6 +19,7 @@ app.use(shopRoutes);
 
 app.use((req, res, next) => {
   res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+  res.status(404).render("404", { docTitle: "404 Not Found" });
 });
 
 app.listen(3000);
