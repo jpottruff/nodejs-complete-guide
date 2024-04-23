99e6a34019355e63b9354c22da08ec7b900a86d9
Jeff Pottruff <jeffpottruff@gmail.com>
2024-04-23 15:49:20 -0400

    add shop boilerplate

--- 04-ExpressJSDynamicContent/04-EJS/routes/shop.js
+++ 04-ExpressJSDynamicContent/04-EJS/routes/shop.js
@@ -1,25 +1,18 @@
-const path = require("path"); // TODO remove
-
 const express = require("express");
 
-const rootDir = require("../util/path"); // TODO remove
 const adminData = require("./admin");
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
-  // TODO remove
-  console.log("from shop.js", adminData.products);
-  res.sendFile(path.join(rootDir, "views", "shop.html"));
-  // TODO
-  // const products = adminData.products;
-  // res.render("shop", {
-  //   productCSS: true,
-  //   docTitle: "Shop",
-  //   activeShop: true,
-  //   path: "/",
-  //   prods: products,
-  //   hasProducts: products.length > 0,
-  // });
+  const products = adminData.products;
+  res.render("shop", {
+    productCSS: true,
+    docTitle: "Shop",
+    activeShop: true,
+    path: "/",
+    prods: products,
+    hasProducts: products.length > 0,
+  });
 });
 
 module.exports = router;
