cec333d570fcfc79ed6dbdbdcbba1a68006c6a81
Jeff Pottruff <jeffpottruff@gmail.com>
2024-04-25 07:53:07 -0400

    move products to use file system

--- 05-MVC/models/product.js
+++ 05-MVC/models/product.js
@@ -1,6 +1,5 @@
-// *NOTE: this would be in server memory and is obviously not ideal
-// It will move to DB later on...
-const products = [];
+const fs = require("fs");
+const path = require("path");
 
 module.exports = class Product {
   constructor(title) {
@@ -8,11 +7,36 @@ module.exports = class Product {
   }
 
   save() {
-    products.push(this);
+    const p = path.join(
+      path.dirname(require.main.filename),
+      "data",
+      "products.json"
+    );
+    fs.readFile(p, (err, data) => {
+      let products = [];
+      // no file content at first
+      if (!err) {
+        products = JSON.parse(data);
+      }
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), (err) => console.log(err));
+    });
   }
 
   // REMEMBER: `static` allows the method to be called on the class itself and not on an instantiated Object
   static fetchAll() {
-    return products;
+    const p = path.join(
+      path.dirname(require.main.filename),
+      "data",
+      "products.json"
+    );
+    // NOTE: this will break things elsewhere...
+    fs.readFile(p, (err, data) => {
+      if (err) {
+        return [];
+      }
+      const products = JSON.parse(data);
+      return products;
+    });
   }
 };
