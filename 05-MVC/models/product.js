7e7d834675ac40d4e4d0bd33c1de9a533c0f1d87
Jeff Pottruff <jeffpottruff@gmail.com>
2024-04-25 08:14:24 -0400

    refactor product model

--- 05-MVC/models/product.js
+++ 05-MVC/models/product.js
@@ -1,41 +1,36 @@
 const fs = require("fs");
 const path = require("path");
 
+const PRODUCTS_DB = path.join(
+  path.dirname(require.main.filename),
+  "data",
+  "products.json"
+);
+
+const getProductsFromFile = (cb) => {
+  fs.readFile(PRODUCTS_DB, (err, data) => {
+    if (err) {
+      return cb([]);
+    }
+    cb(JSON.parse(data));
+  });
+};
 module.exports = class Product {
   constructor(title) {
     this.title = title;
   }
 
   save() {
-    const p = path.join(
-      path.dirname(require.main.filename),
-      "data",
-      "products.json"
-    );
-    fs.readFile(p, (err, data) => {
-      let products = [];
-      // no file content at first
-      if (!err) {
-        products = JSON.parse(data);
-      }
+    getProductsFromFile((products) => {
       products.push(this);
-      fs.writeFile(p, JSON.stringify(products), (err) => console.log(err));
+      fs.writeFile(PRODUCTS_DB, JSON.stringify(products), (err) =>
+        console.log(err)
+      );
     });
   }
 
   // REMEMBER: `static` allows the method to be called on the class itself and not on an instantiated Object
   static fetchAll(cb) {
-    const p = path.join(
-      path.dirname(require.main.filename),
-      "data",
-      "products.json"
-    );
-
-    fs.readFile(p, (err, data) => {
-      if (err) {
-        cb([]);
-      }
-      cb(JSON.parse(data));
-    });
+    getProductsFromFile(cb);
   }
 };
